da48171e01abac15ba35f711a1d5e37b977c0dc8
Ekta Rathi <ekta.rathi@dubitlimited.com>
2016-09-01 09:48:57 +0100

    Updated User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
-    assert_response :success
+    assert_response(200)
   end
 
   test "should get help" do
     get help_path
-    assert_response :success
+    assert_response(200)
   end
 
   test "should get about" do
     get about_path
-    assert_response :success
+    assert_response(200)
   end
 
 end
